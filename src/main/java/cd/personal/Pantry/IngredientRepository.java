7584b1327a2a5024298b3e5fd39ce81af11f93ca
CDaish <cjdaish@outlook.com>
2022-02-02 16:59:34 +0100

    second commit

---
+++ src/main/java/cd/personal/Pantry/IngredientRepository.java
@@ -0,0 +1,9 @@
+package cd.personal.Pantry;
+
+import cd.personal.Pantry.dm.Ingredient;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IngredientRepository extends CrudRepository<Ingredient, Long> {
+}
