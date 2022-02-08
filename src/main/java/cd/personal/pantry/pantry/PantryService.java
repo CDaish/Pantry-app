ea9bc1998b5ca072776468ef7a1b3351ca994102
CDaish <cjdaish@outlook.com>
2022-02-08 09:37:18 +0100

    pantry service to load ingredients and represent stock levels

---
+++ src/main/java/cd/personal/pantry/pantry/PantryService.java
@@ -0,0 +1,34 @@
+package cd.personal.pantry.pantry;
+
+import cd.personal.pantry.ingredient.Ingredient;
+import cd.personal.pantry.ingredient.IngredientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class PantryService {
+
+    private final IngredientRepository ingredientRepository;
+    private Pantry pantry;
+    private Map<Long, Ingredient> ingredientLookupMap;
+
+    public PantryService(IngredientRepository ingredientRepository) {
+        this.ingredientRepository = ingredientRepository;
+    }
+
+    private void loadIngredients() {
+        for (Ingredient ingredient:         ingredientRepository.findAll()
+             ) {
+            ingredientLookupMap.put(ingredient.getId(), ingredient);
+        }
+    }
+
+    public Pantry getPantry() {
+        return pantry;
+    }
+
+    public void setPantry(Pantry pantry) {
+        this.pantry = pantry;
+    }
+}
