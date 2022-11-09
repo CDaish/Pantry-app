3e656d636d7d72f9fbd3c8bf6a646a50e7444d05
CDaish <cjdaish@outlook.com>
2022-11-09 15:53:53 +0100

    recipe repo

--- src/main/java/cd/personal/pantry/recipe/RecipeRepository.java
+++ src/main/java/cd/personal/pantry/recipe/RecipeRepository.java
@@ -5,10 +5,20 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
+import java.util.List;
 
 @Repository
 public interface RecipeRepository extends CrudRepository<Recipe, Long> {
 
     Collection<Recipe> findAllRecipesWithIngredients();
-    Collection<Recipe> findAllRecipesWithIngredientsInStock();
+
+    @Query("select r from Recipe r inner join r.ingredients ingredients where ingredients.amount > ?1")
+    List<Recipe> findByIngredients_AmountGreaterThan(float amount);
+
+    @Query("""
+            select r from Recipe r inner join r.ingredients ingredients
+            where ingredients.amount > ?1 and ingredients.recipeId is not null""")
+    List<Recipe> findByIngredients_AmountAndIngredients_RecipeIdIsNotNull(float amount);
+
+
 }
